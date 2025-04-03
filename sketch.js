ac25d1a01061d1f2881ee0248a22744d3c85f99b
leesubin314 <sbl2580@naver.com>
2025-04-03 13:05:20 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,29 @@
+let circleX = 150;  // 원의 초기 x 위치
+let circleY = 100;  // 원의 초기 y 위치
+let speed = 10;      // 원의 이동 속도
+
 function setup() {
-	createCanvas(400, 400);
-	background(220);
+  createCanvas(400, 400);
+  stroke(0);
+  fill(30);
 }
 
 function draw() {
-	ellipse(mouseX, mouseY, 50, 50);
+  background(200);
+
+  // 키가 눌려져 있으면 원을 움직임
+  if (keyIsPressed) {
+    if (keyCode === LEFT_ARROW) {
+      circleX -= speed;
+    } else if (keyCode === RIGHT_ARROW) {
+      circleX += speed;
+    } else if (keyCode === UP_ARROW) {
+      circleY -= speed;
+    } else if (keyCode === DOWN_ARROW) {
+      circleY += speed;
+    }
+  }
+
+  // 원 그리기
+  ellipse(circleX, circleY, 50, 50);
 }
